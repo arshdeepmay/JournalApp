8591be5ceee1caa818712517fd990cb386337d27
arshdeepmay <19bcs1407@gmail.com>
2025-01-15 23:20:53 +0530

    getbyId , deletebyId , Put/update  all things done

--- src/main/java/net/edigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/edigest/journalApp/controller/JournalEntryController.java
@@ -22,7 +22,24 @@ public class JournalEntryController {
 
     @PostMapping
     public void createEntry(@RequestBody JournalEntry myEntry){
+
         journalEntries.put(myEntry.getId(),myEntry);
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId,myEntry);
     }
 
 }
